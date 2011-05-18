fcc8a3b4b731e8b26d6788448d61f42790415b17
Simão Belchior de Castro <simaobelchior@gmail.com>
2011-05-18 10:09:08 +0100

    changed boot.sh for our 1.9.2 server needs

--- boot.sh
+++ boot.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 set -e -x
+# BOOT SCRIPT BASED ON:
 # Chef-solo bootstrap script for Ubuntu Lucid 10.04 (ami-714ba518)
 # S. Tokumine 2010
+# https://github.com/tokumine/gis-cookbooks/blob/master/boot.sh
+#
+# AND NOW ADAPTED TO DO:
+# Chef-solo bootstrap script for Ubuntu Lucid 10.04 on Brightbox
+# Setting up Ruby 1.9.2 Web+App Server
+# S. Belchior 2011
 #
 # Run using the AWS EC2 API tools:
 #  
@@ -14,52 +21,48 @@ set -e -x
 # UTIL
 # ec2-run-instances --block-device-mapping /dev/sda1=:80 ami-6c06f305 -f boot.sh -k ppekey -g default -g ppeutility -z us-east-1a -m -t c1.medium
 #
-#
-# 32bit box: ami-6c06f305
-# 64bit box: ami-6006f309
-#
-# in ppe production, we run:
-#
-# DB:   m2.xlarge/ami-6006f309/150GB
-# WEB:  c1.medium/ami-6c06f305/80GB  #<-- this will change in the future!
-# UTIL: c1.medium/ami-6c06f305/80GB
-#
-
-# fix bug in 10.04 AMIs
-sudo perl -pi -e 's/(nobootwait),(\S+)/$2,$1/' /etc/fstab
-
-# resize EBS
-resize2fs /dev/sda1
-
-# Brightbox APT repository
-wget http://apt.brightbox.net/release.asc -O - | sudo apt-key add -
-
-# brightbox REE to sources
-echo 'deb http://apt.brightbox.net/ lucid rubyee' > /etc/apt/sources.list.d/brightbox-rubyee.list
 
 # update apt
 aptitude -y update
 aptitude -y safe-upgrade
 
-# install basic packages
-apt-get -y install htop build-essential wget ssl-cert git-core xfsprogs libreadline5-dev checkinstall libruby1.8 ruby ruby1.8-dev libopenssl-ruby1.8 rdoc ri irb
+# remove Ruby 1.8, which comes by default with Brightbox
+apt-get -y purge ruby1.8
 
-# compile rubygems from source
+# install basic packages
+apt-get -y install wget htop git-core checkinstall gcc g++ build-essential libssl-dev libreadline5-dev zlib1g-dev linux-headers-generic, libpq-dev
+
+#### installing Ruby 1.9.2
+
+# install Ruby 1.9.2 from source
 cd /tmp
-wget http://production.cf.rubygems.org/rubygems/rubygems-1.3.6.tgz
-tar zxvf rubygems-1.3.6.tgz
-cd /tmp/rubygems-1.3.6
-ruby setup.rb
-ln -sfv /usr/bin/gem1.8 /usr/bin/gem
+wget ftp://ftp.ruby-lang.org//pub/ruby/1.9/ruby-1.9.2-p0.tar.gz
+tar -xvzf ruby-1.9.2-p0.tar.gz
+cd ruby-1.9.2-p0/
+./configure --prefix=/usr/local/ruby
+make && sudo make install
+
+# add ruby to the PATH
+sed -e '/^PATH/s/"$/:\/usr\/local\/ruby\/bin"/g' -i /etc/environment
+source /etc/environment
+
+# set symbolic links
+sudo ln -s /usr/local/ruby/bin/ruby /usr/local/bin/ruby
+sudo ln -s /usr/local/ruby/bin/gem /usr/bin/gem
+
+# install required gem packages and Rails
+sudo gem install tzinfo builder memcache-client rack rack-test erubis mail text-format bundler thor i18n
+sudo gem install rack-mount --version=0.4.0
+sudo gem install rails
 
 # install chef
 gem sources -a http://gems.opscode.com
-gem install ohai --version "0.5.4" --no-rdoc --no-ri
-gem install chef --version "0.8.16" --no-rdoc --no-ri
+gem install ohai --no-rdoc --no-ri
+gem install chef --no-rdoc --no-ri
   
-# clone tokumine chef repo
+# clone unepwcmc chef repo
 cd /tmp
-git clone http://github.com/tokumine/gis-cookbooks.git
+git clone http://github.com/unepwcmc/cookbooks.git
 cd /tmp/gis-cookbooks
 
 # kick off various different server configs depending on the server
